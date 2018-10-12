e17a3ea426c1a71feb132ec4ab27da15e6af0eb4
freeWillee <will.i.lee@outlook.com>
2018-10-12 15:05:32 -0600

    Update controllers - consolidate goals & assists

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,6 @@
 class GoalsController < ApplicationController
+    get '/user/:username/goals/new' do
+        #Check that user is logged in
+        
+    end
 end
--- app/controllers/goals_target_controller.rb
+++
@@ -1,2 +0,0 @@
-class GoalsTargetController < ApplicationController
-end
