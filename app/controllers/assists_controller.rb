85fdd83eb683ae9a58adcc7efb4e705bd3db389c
freeWillee <will.i.lee@outlook.com>
2018-10-12 15:41:02 -0600

    create basic routes for AssistsController

--- app/controllers/assists_controller.rb
+++ app/controllers/assists_controller.rb
@@ -1,2 +1,19 @@
 class AssistsController < ApplicationController
+    get '/assists/new' do
+        #Check that user is logged in
+        if !logged_in?
+            redirect '/login'
+        else
+            "Render a new assists form"
+        end
+    end
+
+    get '/assists/edit' do
+       #Check that user is logged in
+        if !logged_in?
+            redirect '/login'
+        else
+            "Render an edit assists form"
+        end
+    end
 end
