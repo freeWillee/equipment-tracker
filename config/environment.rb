39419da03c4926972f4f8aea2b88494755314512
freeWillee <will.i.lee@outlook.com>
2018-10-12 15:49:40 -0600

    fixed bug in environment rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
-    :databse => "db/development.sqlite"
+    :database => "db/development.sqlite"
 )
+
 require_all 'app'
